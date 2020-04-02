eb9340d41b15340e244c73e3e529b86bf5f16e5f
Tim La <email@example.com>
2020-04-02 12:44:59 -0400

    progress

--- coinObject.js
+++ coinObject.js
@@ -35,11 +35,13 @@ const coin = {
   }
 };
 function display20Flips() {
+  let storeFlipNumber;
   const results = [];
   for(let i = 0; i < 20; i++){
-   
+   storeFlipNumber = coin.flip()
+   results.push(storeFlipNumber);
   }
-  return results;
+  return results.toString();
   // 4. One point: Use a loop to flip the coin 20 times, each time displaying the result of the flip as a string on the page.  After your loop completes, return an array with the result of each flip.
 }
 function display20Images() {
@@ -50,5 +52,5 @@ function display20Images() {
 console.log(coin.flip());
 console.log(coin.toString());
 coin.toHTML();
-// display20Flips();
+console.log(display20Flips());
 
