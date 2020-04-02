41c91fc74862a175bf8c7bcbb149dbd1e5958616
Tim La <email@example.com>
2020-04-02 14:16:37 -0400

    progress

--- coinObject.js
+++ coinObject.js
@@ -1,3 +1,6 @@
+let outputImage = document.querySelector("#coinFlipImage");
+let holding20FlipsCounts = document.querySelector("#flipped20Times");
+
 const coin = {
   state: 0,
   flip: function() {
@@ -22,24 +25,26 @@ const coin = {
       if(this.state === "Heads"){
         imageHeads.src="images/quarter-heads.png";
         imageHeads.style.width = "350px"
-        document.body.appendChild(imageHeads);
+        outputImage.appendChild(imageHeads);
         return imageHeads;
       }else if(this.state === "Tails"){
         imageTails.src="images/quarter-tails.png";
         imageTails.style.width = "350px"
-        document.body.appendChild(imageTails);
+        outputImage.appendChild(imageTails);
         return imageTails;
       }
       // 3. One point: Set the properties of this image element to show either face-up
       //    or face-down, depending on whether this.state is 0 or 1.
   }
 };
+
 function display20Flips() {
   let storeFlipNumber;
   const results = [];
   for(let i = 0; i < 20; i++){
     storeFlipNumber = coin.flip()
     results.push(storeFlipNumber);
+    // holding20FlipsCounts.appendChild(results);
   }
   return results.toString();
   // 4. One point: Use a loop to flip the coin 20 times, each time displaying the result of the flip as a string on the page.  After your loop completes, return an array with the result of each flip.
@@ -59,6 +64,8 @@ console.log(coin.toString());
 coin.toHTML();
 console.log(display20Flips());
 console.log(display20Images());
+
+display20Flips();
 
 
 
